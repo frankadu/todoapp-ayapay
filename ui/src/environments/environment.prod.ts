d6113586abe7285b526f638d143fc234fe3af0c6
System Administrator <root@users-MacBook-Pro-2.local>
2020-01-12 17:06:28 +0100

    Initial Commit

---
+++ ui/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  gateway: '',
+  callback: 'http://localhost:4200/callback',
+  domain: 'https://dev-lyp73-mq.auth0.com/',
+  clientId: '5e1b3c72a7eadd0779645e24',
+  audience: 'https://my-todoapp-api'
+};
---
+++ ui/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  gateway: 'http://localhost:3000',
+  callback: 'http://localhost:4200/callback',
+  domain: 'https://dev-lyp73-mq.auth0.com/',
+  clientId: '5e1b3c72a7eadd0779645e24',
+  audience: 'https://my-todoapp-api'
+};
